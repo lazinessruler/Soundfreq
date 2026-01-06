f65c464f1c059b33d2e282ba2a2825f22a9715cb
𝖸𝖺𝗁𝗂𝗄𝗈 !! <lazinessruler@gmail.com>
2026-01-07 03:14:28 +0530

    Update mmf.py

--- AviaxMusic/plugins/tools/mmf.py
+++ AviaxMusic/plugins/tools/mmf.py
@@ -1,22 +1,9 @@
-# -----------------------------------------------
-# 🔸 StrangerMusic Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
-# 📅 Copyright © 2022 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by ItzShukla
-# -----------------------------------------------
 import os
 import textwrap
 from PIL import Image, ImageDraw, ImageFont
 from pyrogram import filters
 from pyrogram.types import Message
-from SHUKLAMUSIC import app
+from AviaxMusic import app
 
 @app.on_message(filters.command("mmf"))
 async def mmf(_, message: Message):
@@ -49,7 +36,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "arial.ttf"
     else:
-        fnt = "./SHUKLAMUSIC/assets/default.ttf"
+        fnt = "./AviaxMusic/assets/default.ttf"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
